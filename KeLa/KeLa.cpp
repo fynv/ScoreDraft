386ab7d92b4ab702bdd9452fc02a8f0602ef9ff2
fynv <feiy@nvidia.com>
2018-01-14 10:59:14 +0800

    KeLa engine minor adjustment

--- KeLa/KeLa.cpp
+++ KeLa/KeLa.cpp
@@ -34,7 +34,7 @@ struct SymmetricWindowWithPosition
 };
 
 
-void DetectFreqs(const Buffer& buf, std::vector<float>& frequencies, unsigned step, float& ave_freq)
+void DetectFreqs(const Buffer& buf, std::vector<float>& frequencies, unsigned step)
 {
 	unsigned halfWinLen = 1024;
 	float* temp = new float[halfWinLen * 2];
@@ -93,8 +93,6 @@ void DetectFreqs(const Buffer& buf, std::vector<float>& frequencies, unsigned st
 		}
 	}
 
-	ave_freq = 0.0f;
-	float count = 0.0f;
 	unsigned sampleBegin = (unsigned) (-1);
 	size_t sampleEnd = (unsigned)(-1);
 	for (size_t i = 0; i < frequencies.size(); i++)
@@ -109,13 +107,7 @@ void DetectFreqs(const Buffer& buf, std::vector<float>& frequencies, unsigned st
 				sampleBegin = (unsigned)i;
 			sampleEnd = (unsigned)i+1;
 		}		
-		if (frequencies[i] > 0.0f)
-		{
-			ave_freq += frequencies[i];
-			count += 1.0f;
-		}
 	}
-	ave_freq /= count;
 
 	for (unsigned i = sampleBegin; i < sampleEnd; i++)
 		if (frequencies[i] < 0.0f) frequencies[i] = 0.0f;
@@ -280,14 +272,11 @@ private:
 
 		unsigned freq_step = 256;
 		std::vector<float> frequencies;
-		float ave_freq;
-
+		
 		sprintf(path, "KeLaSamples/%s/%s.freq", m_name.data(), lyric);
 		FILE* fp = fopen(path, "r");
 		if (fp)
 		{
-			ave_freq = 0.0f;
-			float count = 0.0f;
 			while (!feof(fp))
 			{
 				float f;
@@ -296,20 +285,12 @@ private:
 					frequencies.push_back(f);
 				}
 				else break;
-
-				if (f > 0.0f)
-				{
-					ave_freq += f;
-					count += 1.0f;
-				}
 			}
 			fclose(fp);
-
-			ave_freq /= count;
 		}
 		else
 		{
-			DetectFreqs(source, frequencies, freq_step, ave_freq);
+			DetectFreqs(source, frequencies, freq_step);
 			fp = fopen(path, "w");
 			for (size_t i = 0; i < frequencies.size(); i++)
 			{
@@ -355,153 +336,123 @@ private:
 		}
 		if (voicedEnd == (unsigned)(-1))
 		{
+			voicedEnd_id = (unsigned)frequencies.size();
 			voicedEnd = (unsigned)source.m_data.size();
-			lastFreq = frequencies[frequencies.size() - 1];
+			lastFreq = frequencies[voicedEnd_id - 1];
 		}
 		if (unvoicedEnd == (unsigned)(-1))
 		{
 			unvoicedEnd = (unsigned)source.m_data.size();
 		}
 
-		std::vector<SymmetricWindow> windows;
+		unsigned voicedLen = voicedEnd - voicedBegin;
+		unsigned totalLen = unvoicedEnd - unvoicedBegin;
+		unsigned unvoicedLen = totalLen - voicedLen;
+		float voiced_portion = (float)voicedLen / (float)totalLen;
+
+		float voicedWeight;
+		float unvoicedWeight;
+
+		if (voiced_portion < 0.9f)
+		{
+			float k = voiced_portion / 9.0f;
+			voicedWeight = 1.0f / (k* unvoicedLen + voicedLen);
+			unvoicedWeight = k* voicedWeight;
+		}
+		else
+		{
+			voicedWeight = unvoicedWeight = 1.0f / (float)totalLen;
+		}
+
+		class SymmetricWindowWithPosition : public SymmetricWindow
+		{
+		public:
+			float m_pos;
+		};
+
+		std::vector<SymmetricWindowWithPosition> windows;
 		float fPeriodCount = 0.0f;
-		for (unsigned srcPos = voicedBegin; srcPos < voicedEnd; srcPos++)
+		float logicalPos = 0.0f;
+		for (unsigned srcPos = unvoicedBegin; srcPos < unvoicedEnd; srcPos++)
 		{
 			float srcFreqPos = (float)srcPos / (float)freq_step;
 			unsigned uSrcFreqPos = (unsigned)srcFreqPos;
 			float fracSrcFreqPos = srcFreqPos - (float)uSrcFreqPos;
 
-			float srcFreq;
-			if (uSrcFreqPos >= frequencies.size() - 1)
-			{
-				if (frequencies.size() - 1 >= voicedEnd_id) srcFreq = lastFreq;
-				else srcFreq = frequencies[frequencies.size() - 1];
-			}
-			else
-			{
-				float freq1;
-				if (uSrcFreqPos < voicedBegin_id) freq1 = firstFreq;
-				else if (uSrcFreqPos >= voicedEnd_id) freq1 = lastFreq;
-				else freq1 = frequencies[uSrcFreqPos];
+			float sampleFreq1;
+			if (uSrcFreqPos < voicedBegin_id) sampleFreq1 = freqMap[0];
+			else if (uSrcFreqPos >= voicedEnd_id) sampleFreq1 = freqMap[uSumLen - 1];
+			else sampleFreq1 = frequencies[uSrcFreqPos] / (float)source.m_sampleRate;
 
-				float freq2;
-				if (uSrcFreqPos + 1 < voicedBegin_id) freq2 = firstFreq;
-				else if (uSrcFreqPos + 1 >= voicedEnd_id) freq2 = lastFreq;
-				else freq2 = frequencies[uSrcFreqPos + 1];
+			float sampleFreq2;
+			if (uSrcFreqPos + 1 < voicedBegin_id) sampleFreq2 = freqMap[0];
+			else if (uSrcFreqPos + 1 >= voicedEnd_id) sampleFreq2 = freqMap[uSumLen - 1];
+			else sampleFreq2 = frequencies[uSrcFreqPos + 1] / (float)source.m_sampleRate;
 
-				srcFreq = freq1*(1.0f - fracSrcFreqPos) + freq2*fracSrcFreqPos;
-			}
+			float srcSampleFreq = sampleFreq1*(1.0f - fracSrcFreqPos) + sampleFreq2*fracSrcFreqPos;
 
 			unsigned winId = (unsigned)fPeriodCount;
 			if (winId >= windows.size())
 			{
-				float srcHalfWinWidth = (float)source.m_sampleRate / srcFreq;
+				float srcHalfWinWidth = 1.0f / srcSampleFreq;
 				Window srcWin;
 				srcWin.CreateFromBuffer(source, (float)srcPos, srcHalfWinWidth);
 
-				SymmetricWindow symWin;
+				SymmetricWindowWithPosition symWin;
 				symWin.CreateFromAsymmetricWindow(srcWin);
+				symWin.m_pos = logicalPos;
 
 				windows.push_back(symWin);
+
 			}
-
-			fPeriodCount += srcFreq / (float)source.m_sampleRate;
-
+			fPeriodCount += srcSampleFreq;
+			if (srcPos < voicedBegin || srcPos >= voicedEnd)
+			{
+				logicalPos += unvoicedWeight;
+			}
+			else
+			{
+				logicalPos += voicedWeight;
+			}
 		}
 
 		float tempLen = stretchingMap[uSumLen - 1];
 		unsigned uTempLen = (unsigned)ceilf(tempLen);
+
 		Buffer tempBuf;
 		tempBuf.m_sampleRate = source.m_sampleRate;
 		tempBuf.m_data.resize(uTempLen);
 		tempBuf.SetZero();
 
-		float rateUnvoiced0 = minSampleFreq * (float)source.m_sampleRate / firstFreq;
-		float lenUnvoiced0 = (float)(voicedBegin - unvoicedBegin) / rateUnvoiced0;
-
-		float rateUnvoiced1 = minSampleFreq * (float)source.m_sampleRate / lastFreq;
-		float lenUnvoiced1 = (float)(unvoicedEnd - voicedEnd) / rateUnvoiced1;
-
-		if (lenUnvoiced0 > tempLen)
-		{
-			lenUnvoiced0 = tempLen;
-			lenUnvoiced1 = 0.0f;
-		}
-
-		if (lenUnvoiced0 + lenUnvoiced1 > tempLen)
-			lenUnvoiced1 = tempLen - lenUnvoiced0;
-
-		float lenVoiced = tempLen - lenUnvoiced0 - lenUnvoiced1;
-
 		float tempHalfWinLen = 1.0f / minSampleFreq;
 
-		// unvoiced 0
-		float srcPos = (float)unvoicedBegin;
-		bool interpolation = rateUnvoiced0 < 1.0f;
-		for (unsigned uTmpPos = 0; (float)uTmpPos < lenUnvoiced0 && uTmpPos < uTempLen; uTmpPos++, srcPos += rateUnvoiced0)
+		unsigned winId0 = 0;
+		unsigned pos_final = 0;
+		
+		for (float fTmpWinCenter = 0.0f; fTmpWinCenter <= tempLen; fTmpWinCenter += tempHalfWinLen)
 		{
-			float tmpValue;
-			if (interpolation)
-			{
-				int ipos1 = (int)floorf(srcPos);
-				float frac = srcPos - (float)ipos1;
-				int ipos2 = ipos1 + 1;
-				int ipos0 = ipos1 - 1;
-				int ipos3 = ipos1 + 2;
+			float fWinPos = fTmpWinCenter / tempLen;
+			while (winId0<windows.size() - 1 && windows[winId0].m_pos < fWinPos) winId0++;
 
-				float p0 = source.GetSample(ipos0);
-				float p1 = source.GetSample(ipos1);
-				float p2 = source.GetSample(ipos2);
-				float p3 = source.GetSample(ipos3);
+			unsigned winId1 = winId0 + 1;
 
-				tmpValue = (-0.5f*p0 + 1.5f*p1 - 1.5f*p2 + 0.5f*p3)*powf(frac, 3.0f) +
-					(p0 - 2.5f*p1 + 2.0f*p2 - 0.5f*p3)*powf(frac, 2.0f) +
-					(-0.5f*p0 + 0.5f*p2)*frac + p1;
-			}
+			if (winId1 == windows.size()) winId1 = winId0;
+			SymmetricWindowWithPosition& win0 = windows[winId0];
+			SymmetricWindowWithPosition& win1 = windows[winId1];
+
+			float k;
+			if (fTmpWinCenter >= win1.m_pos) k = 1.0f;
 			else
 			{
-				int ipos1 = (int)ceilf(srcPos - rateUnvoiced0*0.5f);
-				int ipos2 = (int)floorf(srcPos + rateUnvoiced0*0.5f);
-
-				float sum = 0.0f;
-				for (int ipos = ipos1; ipos <= ipos2; ipos++)
-				{
-					sum += source.GetSample(ipos);
-				}
-				tmpValue = sum / (float)(ipos2 - ipos1 + 1);
-
+				k = (fTmpWinCenter - win0.m_pos) / (win1.m_pos - win0.m_pos);
 			}
-
-			float ampl = 1.0f;
-			if (uTmpPos>lenUnvoiced0 - tempHalfWinLen)
-			{
-				float progress = (uTmpPos - (lenUnvoiced0 - tempHalfWinLen)) / tempHalfWinLen;
-				ampl = (cosf(progress * (float)PI) + 1.0f)*0.5f;
-			}
-
-			tempBuf.m_data[uTmpPos] = tmpValue*ampl;
-		}
-
-		// voiced
-		float fTmpWinCenter;
-		unsigned pos_final = 0;
-
-		for (fTmpWinCenter = lenUnvoiced0; fTmpWinCenter < lenUnvoiced0 + lenVoiced; fTmpWinCenter += tempHalfWinLen)
-		{
-			float fWinId = (float)windows.size()* (fTmpWinCenter - lenUnvoiced0) / lenVoiced;
-			unsigned winId0 = min((unsigned)fWinId, (unsigned)windows.size() - 1);
-			unsigned winId1 = min(winId0 + 1, (unsigned)windows.size() - 1);
-			float k = fWinId - (float)winId0;
 
 			while (fTmpWinCenter > stretchingMap[pos_final]) pos_final++;
 
 			float destSampleFreq = freqMap[pos_final];
 			float destHalfWinLen = 1.0f / destSampleFreq;
 
-			SymmetricWindow& win0 = windows[winId0];
 			SymmetricWindow shiftedWin0;
-
-			SymmetricWindow& win1 = windows[winId1];
 			SymmetricWindow shiftedWin1;
 
 			SymmetricWindow l_win;
@@ -534,54 +485,8 @@ private:
 			{
 				l_win2.Scale(*destWin, tempHalfWinLen);
 			}
+
 			winToMerge->MergeToBuffer(tempBuf, fTmpWinCenter);
-
-		}
-
-		// unvoiced 1
-		float unvoicedBegin1 = fTmpWinCenter - tempHalfWinLen;
-		srcPos = (float)voicedEnd;
-		for (unsigned uTmpPos = (unsigned)ceilf(unvoicedBegin1); uTmpPos < uTempLen; uTmpPos++, srcPos += rateUnvoiced1)
-		{
-			float tmpValue;
-			if (interpolation)
-			{
-				int ipos1 = (int)floorf(srcPos);
-				float frac = srcPos - (float)ipos1;
-				int ipos2 = ipos1 + 1;
-				int ipos0 = ipos1 - 1;
-				int ipos3 = ipos1 + 2;
-
-				float p0 = source.GetSample(ipos0);
-				float p1 = source.GetSample(ipos1);
-				float p2 = source.GetSample(ipos2);
-				float p3 = source.GetSample(ipos3);
-
-				tmpValue = (-0.5f*p0 + 1.5f*p1 - 1.5f*p2 + 0.5f*p3)*powf(frac, 3.0f) +
-					(p0 - 2.5f*p1 + 2.0f*p2 - 0.5f*p3)*powf(frac, 2.0f) +
-					(-0.5f*p0 + 0.5f*p2)*frac + p1;
-			}
-			else
-			{
-				int ipos1 = (int)ceilf(srcPos - rateUnvoiced0*0.5f);
-				int ipos2 = (int)floorf(srcPos + rateUnvoiced0*0.5f);
-
-				float sum = 0.0f;
-				for (int ipos = ipos1; ipos <= ipos2; ipos++)
-				{
-					sum += source.GetSample(ipos);
-				}
-				tmpValue = sum / (float)(ipos2 - ipos1 + 1);
-
-			}
-
-			float ampl = 1.0f;
-			if (uTmpPos < unvoicedBegin1 + tempHalfWinLen)
-			{
-				float progress = (uTmpPos - (unvoicedBegin1 + tempHalfWinLen)) / tempHalfWinLen;
-				ampl = (cosf(progress * (float)PI) + 1.0f)*0.5f;
-			}
-			tempBuf.m_data[uTmpPos] += tmpValue*ampl;
 		}
 
 		// post processing
@@ -612,7 +517,9 @@ private:
 			noteBuf->m_data[pos] = amplitude*value*multFac;
 		}
 
+
 		delete[] stretchingMap;
+
 	}
 
 	std::string m_name;
