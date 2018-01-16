da83806bdb82f83c7b0a19fe17c6341bd5e8acd7
fynv <feiy@nvidia.com>
2018-01-16 21:43:16 +0800

    Qt test continue

--- QtPCMPlayer/main.cpp
+++ QtPCMPlayer/main.cpp
@@ -1,11 +1,30 @@
 #include <qapplication.h>
+#include <QtNetwork/QLocalSocket>
+#include <QtNetwork/QLocalServer>
 #include "QtPCMPlayer.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+
+	QLocalSocket socket;
+	socket.connectToServer("QtPCMPlayer");
+
+	if (socket.waitForConnected(500))
+	{
+		socket.close();
+		return -1;
+	}
+
+	QLocalServer server;
+	if (!server.listen("QtPCMPlayer")) return -1;
+
 	QtPCMPlayer player;
 	player.show();
 
-	return app.exec();
+	int res= app.exec();
+
+	server.close();
+
+	return res;
 }
