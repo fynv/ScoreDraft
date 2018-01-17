34f988582b01774f3fcbf25266a794c1309104d5
fynv <feiy@nvidia.com>
2018-01-17 08:20:33 +0800

    RapTest adjust rhythm

--- python_test/RapTest.py
+++ python_test/RapTest.py
@@ -18,9 +18,11 @@ GePing= ScoreDraft.GePing()
 #seq += [ ("ting", 2, 48), ("tai",2, 48), ("liu",4,24), ("qi", 1,24), ("zuo", 4, 48)]
 #seq += [ ("ba", 1, 24), ("jiu",3, 24), ("shi",2,24), ("zhi", 1,24), ("hua", 1, 48), BL(48)]
 
-durations=[ [48,48,24,24,48, 24,24,24,24,48] ]
+#durations=[ [48,48,24,24,48, 24,24,24,24,48] ]
 
-poem=poems[17]
+durations=[ [24,36,32,32,36, 24,36,32,32,36] ]
+
+poem=poems[1]
 divider= poem[0]*2
 
 assert(divider==10)
@@ -28,9 +30,12 @@ assert(divider==10)
 seq=[]
 
 for i in range(int(len(poem[1])/divider)):
-	for j in range(10):
-		seq+=[(poem[1][i*10+j][0],poem[1][i*divider+j][1], durations[0][j])]
-	seq+=[BL(48)]
+	for j in range(5):
+		seq+=[(poem[1][i*divider+j][0],poem[1][i*divider+j][1], durations[0][j])]
+	seq+=[BL(32)]
+	for j in range(5,10):
+		seq+=[(poem[1][i*divider+j][0],poem[1][i*divider+j][1], durations[0][j])]
+	seq+=[BL(32)]
 
 doc.sing(seq, ScoreDraft.GePing())
 
