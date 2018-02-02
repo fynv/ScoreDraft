cc5f9444024997665854e2716f45fde7d8435a60
fynv <feiy@nvidia.com>
2018-02-02 19:31:02 +0800

    update jupyter slides

--- python_test/Examples/uta_test.py
+++ python_test/Examples/uta_test.py
@@ -11,11 +11,12 @@ doc=ScoreDraft.Document()
 doc.setTempo(120)
 
 seq= [ ('あ', mi(5,48), 'り', so(5,48), 'が', ti(5,48), 'とぅ', do(6,144), ti(5,144), so(5,144))]
-#doc.sing(seq, ScoreDraft.uta_UTAU())
+doc.sing(seq, ScoreDraft.uta_UTAU())
 
+'''
 Ayaka= ScoreDraft.Ayaka2_UTAU()
 ScoreDraft.UtauDraftSetLyricConverter(Ayaka, JPVCVConverter.JPVCVConverter)
 doc.sing(seq, Ayaka)
-
+'''
 
 doc.mixDown('uta.wav')
