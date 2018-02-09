b3739fe1a4a670123af0fb957dc47195ed61b863
feiy <feiy@nvidia.com>
2018-02-09 17:10:12 +0800

    Adding "TTEnglishConverter"

---
+++ python_test/TTEnglishConverter.py
@@ -0,0 +1,177 @@
+import os
+import pickle
+ScoreDraftPath= os.path.dirname(__file__)
+
+lyricSet=set()
+lyricPrefixSet=set()
+vowelSet={'eI','aI','aU','OI','oU','3','i','I','U','u','E','{','A','V','O','@'}
+vowelPrefixSet=set()
+
+def BuildLyricSet():
+	with open(ScoreDraftPath+'/UTAUVoice/TetoEng/Teto/oto.ini', 'r') as f:
+		while True:
+			line = f.readline()
+			if not line:
+				break
+			p1 = line.find("=")
+			if p1==-1:
+				continue
+			fn=line[0:p1-4]
+			p2 = line.find(',',p1)
+			if p2==-1:
+				continue
+			lyric=line[p1+1:p2]
+			if lyric=='':
+				lyric=fn
+			lyricSet.add(lyric)
+	with open(ScoreDraftPath+'/TTLyricSet.data','wb') as f:
+		pickle.dump(lyricSet,f)
+
+def LoadLyricSet():
+	with open(ScoreDraftPath+'/TTLyricSet.data','rb') as f:
+		global lyricSet
+		lyricSet=pickle.load(f)
+
+#BuildLyricSet()
+LoadLyricSet()
+
+for lyric in lyricSet:
+	for i in range(len(lyric)):
+		if lyric[i]==' ':
+			continue
+		lyricPrefixSet.add(lyric[0:i+1])
+
+for vowel in vowelSet:
+	for i in range(len(vowel)):
+		vowelPrefixSet.add(vowel[0:i+1])
+
+def TTEnglishConverter(inList):
+	vowelMap=[]
+
+	for inLyric in inList:
+		start=-1
+		end=-1
+		for i in range(len(inLyric)):
+			if start==-1:
+				if inLyric[i] in vowelPrefixSet:
+					start=i
+			if inLyric[start:i+1] in vowelPrefixSet:
+				end=i+1
+		vowelMap+=[(start,end)]
+
+	cur=[0,0]
+	outList=[]
+
+	prefix='-'
+	iIn=0
+
+
+	while cur[0]<len(inList) and cur[1]<len(inList[cur[0]]):	
+		# pass 1 
+		while len(prefix)>0:
+			test_seg = prefix + inList[cur[0]][cur[1]]
+
+			if test_seg in lyricPrefixSet:
+				break
+
+			test_seg = prefix +' '+inList[cur[0]][cur[1]]
+
+			if test_seg in lyricPrefixSet:
+				break
+
+			prefix=prefix[1:len(prefix)]
+
+		#pass2
+		nextStart=cur[:]
+		seg=''
+		isVowel=False
+
+		while True:
+			seg=''
+			lastSeg=prefix[:]
+			cur2=cur[:] 
+
+			isVowel=False
+
+			while True:
+				newChar=''
+				if not (cur2[0]<len(inList) and cur2[1]<len(inList[cur2[0]])):
+					newChar='-'
+				else:
+					newChar= inList[cur2[0]][cur2[1]]
+
+				test_seg=lastSeg+newChar
+				if not (test_seg in lyricPrefixSet):
+					test_seg=lastSeg+' '+newChar
+					if not (test_seg in lyricPrefixSet):
+						break
+
+				lastSeg=test_seg
+
+				if test_seg in lyricSet:
+					cur=cur2[:]
+					seg=test_seg
+					if cur[0]<len(inList):
+						if cur[1]>=vowelMap[cur[0]][0] and cur[1]<vowelMap[cur[0]][1]:
+							isVowel=True
+							iIn=cur[0]
+
+				if not (cur2[0]<len(inList) and cur2[1]<len(inList[cur2[0]])):
+					break
+
+				cur2[1]+=1
+				if cur2[1]>=len(inList[cur2[0]]):
+					cur2[0]+=1
+					cur2[1]=0				
+
+
+			if len(seg)>0 or len(prefix)==0:
+				break
+			
+			prefix=prefix[1:len(prefix)]	
+
+		outList+=[(seg, iIn, isVowel)]
+
+		if not (cur[0]<len(inList) and cur[1]<len(inList[cur[0]])):
+			break
+
+		cur[1]+=1
+		if cur[1]>=len(inList[cur[0]]):
+			cur[0]+=1
+			cur[1]=0
+
+		pos=nextStart[:]
+		prefix=''
+		while pos[0]<cur[0] or (pos[0]==cur[0] and pos[1]<cur[1]):
+			prefix+=inList[pos[0]][pos[1]]
+			pos[1]+=1
+			if pos[1]>=len(inList[pos[0]]):
+				pos[0]+=1
+				pos[1]=0
+
+	ret=[]
+	syllable=()
+	iSyllable=0
+
+	for i in range(len(outList)):
+		outItem=outList[i]
+		if outItem[1]!=iSyllable:
+			ret+=[syllable]
+			syllable=()
+			iSyllable=outItem[1]
+		weight=0.1
+		if outItem[2]:
+			weight=0.4
+		syllable+=(outItem[0], weight)
+
+	ret+=[syllable]
+
+	#print(ret)
+
+	return ret
+
+
+
+
+
+
