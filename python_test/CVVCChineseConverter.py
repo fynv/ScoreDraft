ecf8a37c705e7fb411bdeb51fadbcb66c27480ae
fynv <feiy@nvidia.com>
2018-05-12 22:18:16 +0800

    The correct fix to CVVCChineseConverter

--- python_test/CVVCChineseConverter.py
+++ python_test/CVVCChineseConverter.py
@@ -80,14 +80,5 @@ def CVVCChineseConverter(LyricForEachSyllable):
 			else:
 				ret+=[(lyric,1.0, True)]
 		else:
-			if CV[i][1]=='ai' or CV[i][1]=='ei':
-				ret+=[(lyric,0.875, True, CV[i][1]+" y", 0.125, False)]
-			elif CV[i][1]=='ou':
-				ret+=[(lyric,0.875, True, CV[i][1]+" w", 0.125, False)]
-			elif CV[i][1]=='an' or CV[i][1]=='en' or CV[i][1]=='in':
-				ret+=[(lyric,0.875, True, CV[i][1]+" n", 0.125, False)]
-			elif CV[i][1]=='ang' or CV[i][1]=='eng' or CV[i][1]=='ing' or CV[i][1]=='ing':
-				ret+=[(lyric,0.875, True, CV[i][1]+" n", 0.125, False)]
-			else:
-				ret+=[(lyric,1.0, True)]
+			ret+=[(lyric,0.875, True, CV[i][1]+" R", 0.125, False)]
 	return ret
